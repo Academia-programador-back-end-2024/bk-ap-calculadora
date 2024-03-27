8c876f26c1e136b283a78bfd70f8ff954ac4f156
George Hamann <george.hamann@gmail.com>
2024-03-26 21:11:03 -0300

    Código feito na aula para resolver os outros requesitos.

--- src/Calculadora.Console/Program.cs
+++ src/Calculadora.Console/Program.cs
@@ -1,48 +1,72 @@
 ﻿//Requisito 01 
 //Nossa calculadora deve ter a possibilidade de somar dois números
 
-int primeiroNumerador;//camelCase
-int segundoNumerador;
+decimal primeiroNumerador;//camelCase
+decimal segundoNumerador;// decimal
 
-int SubtracaoNumeros;//PascalCase
-int SomaNumeros;//PascalCase
+decimal ResultadoOperacao = 0;
 
 string saida = "exit";
-string operacao;
-do
+string operacao = "";
+
+while (operacao != saida)
 {
-    Console.WriteLine("Digite o primeiro valor:");
-    primeiroNumerador = Convert.ToInt16(Console.ReadLine());
+    Console.Clear();
 
-    Console.WriteLine("Digite o segundo valor:");
-    segundoNumerador = Convert.ToInt16(Console.ReadLine());
-
-
-    Console.WriteLine("Deseja fazer uma soma? (add)");
-    Console.WriteLine("Deseja fazer uma subtracao? (sub)");
+    Console.WriteLine("Para fazer uma soma digite 1");
+    Console.WriteLine("Para fazer uma subtracao digite 2");
+    Console.WriteLine("Para fazer uma multiplicação digite 3");
+    Console.WriteLine("Para fazer uma divisão digite 4");
     Console.WriteLine("Para sair, digite 'exit'");
 
     operacao = Console.ReadLine();
 
+    if( operacao == saida)
+    {
+        //Faz sair do laço de repetição
+        break;
+    }
 
-    if (operacao == "add")
-    {
-        SomaNumeros = primeiroNumerador + segundoNumerador;
-        Console.WriteLine("Soma dos valores :" + SomaNumeros);
-    }
-    else if (operacao == "sub")
-    {
-        SubtracaoNumeros = primeiroNumerador - segundoNumerador;
-        Console.WriteLine("Subtração dos valores :" + SubtracaoNumeros);
-    }
-    else
+    if(Convert.ToInt16(operacao) > 4) // >= != < <= // || ou &&
     {
         Console.WriteLine("Nao reconheço essa operacao");
+        continue;
     }
-} while (operacao != saida);
+
+    Console.WriteLine("Digite o primeiro valor:");
+    primeiroNumerador = Convert.ToDecimal(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor:");
+    segundoNumerador = Convert.ToDecimal(Console.ReadLine());
+    switch (operacao)
+    {
+        case "1"://Somar
+            ResultadoOperacao = primeiroNumerador + segundoNumerador;
+            break;
+        case "2"://Subtração
+            ResultadoOperacao = primeiroNumerador - segundoNumerador;
+            break;
+        case "3"://Multiplicação
+            ResultadoOperacao = primeiroNumerador * segundoNumerador;
+            break;
+        case "4"://Divisão
+            ResultadoOperacao = primeiroNumerador / segundoNumerador;
+            break;
+        default:
+            Console.WriteLine("Nao reconheço essa operacao");
+            break;
+    }
+    Console.WriteLine("Resultado da operação escolhida é de :" + ResultadoOperacao);
+
+};
 
 
 //Requisito 02 
 //Nossa calculadora deve ter a possibilidade de fazer várias operações de soma
 //Requisito 03 
 //Nossa calculadora deve ter a possibilidade fazer várias operações de soma e de subtração
+//Requisito 04 
+//Nossa calculadora deve ter a possibilidade fazer as quatro operações básicas da matemática
+//07 Nossa calculadora deve realizar as operações com números com duas casas decimais
+
+//Nossa calculadora deve validar a opções do menu 
